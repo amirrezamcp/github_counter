ba6823aa2b2f8827bbd2d05ad268b59e505cb06e
amirrezamcp <yt.bigpotato@gmail.com>
2024-05-04 22:32:56 +0330

    created Databas

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+use Classes\Database;
 require_once "Autoloader.php";
-use Classes\TestClass;
+// use Classes\TestClass;
 
-$obj = new TestClass();
-$obj->test('amir');
+// $obj = new TestClass();
+// $obj->test('amir');
+
+$dbs = new Database();
+$data = [
+    'name' => 'admin',
+    'email' => 'admin@example.com',
+    'password' => md5('admin!@#123456789')
+];
+$result = $dbs->insert('users', $data);
+var_dump($result);
