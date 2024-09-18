9659571149fb35558532927e4b77601aee4c1f6a
amirrezamcp <mousavi.amirreza30@gmail.com>
2024-09-18 09:44:49 +0330

    index page

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
 
-require_once "Autoloader.php";
-
-use Classes\Database;
-
-// use Classes\TestClass;
-
-// $obj = new TestClass();
-// $obj->test('amir');
-
-$dbs = new Database();
-$data = [
-    'name' => 'admin',
-    'email' => 'admin@example.com',
-    'password' => md5('admin!@#123456789')
-];
-$result = $dbs->insert('users', $data);
-var_dump($result);
+require_once 'Autoloader.php';
