c8193bb3aea5e81a0fd03cd897660e949aefed8f
amirrezamcp <mousavi.amirreza30@gmail.com>
2024-12-15 11:15:50 +0330

    create default pages

--- pages/Default.php
+++ pages/Default.php
@@ -1,1 +1,27 @@
-<h1>Default page.</h1>
+<div class="row">
+    <div class="col-12">
+        <h2>Links</h2>
+        <a href="<?= htmlspecialchars($_SERVER['PHP_SELF']) . '?page=newLinks'; ?>">
+            <button class="btn btn-primary">Create New Link</button>
+        </a>
+        <hr>
+        <table class="table table-stripped table-hover">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>UUID</th>
+                    <th>View</th>
+                    <th>Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>test 1</th>
+                    <th>a1b2c3</th>
+                    <th>+9999</th>
+                    <th>Admin</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
